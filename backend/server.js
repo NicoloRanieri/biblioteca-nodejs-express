79d50148459b5ae22f0017e373b9ffcce8960caa
NicoloRanieri <nicolo.ranieri@itsmeccanicabruzzo.eu>
2026-03-28 19:39:23 +0100

    aggiunta put e delete

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,42 @@ app.post("/libri", async (req, res) => {
   }
 });
 
+//modifica di un libro in particolare
+app.put("/libri/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: "Errore nella modifica del libro" });
+  }
+});
+
+//eliminazione di un libro
+app.delete("/libri/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "DELETE"
+    });
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: "Errore nell'eliminazione del libro" });
+  }
+});
+
 
 
 // Avvio del server
